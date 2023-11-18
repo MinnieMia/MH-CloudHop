239cafc957fdae1880e291cc7d4a08d01eefaaa0
Andrewkk0139 <130806226+MinnieMia@users.noreply.github.com>
2023-11-18 13:55:04 -0600

    step 8a

--- MH-CloudHop/Assets/Scripts/Jump.cs
+++ MH-CloudHop/Assets/Scripts/Jump.cs
@@ -4,10 +4,11 @@ using UnityEngine;
 
 public class Jump : MonoBehaviour
 {
+    Rigidbody2D rigidbody;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rigidbody = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
